426930fa4d4beebf76557711ff3792088ae3db93
Ghazaaaaa <gymnastiarghaza@gmail.com>
2024-04-02 12:56:22 +0700

    Profile

--- lib/profile.dart
+++ lib/profile.dart
@@ -9,14 +9,25 @@ class ProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Profile'),
-        backgroundColor: Colors.red,
+        title: const Text(
+          'Profile',
+          style: TextStyle(
+            color: Colors.red,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
         leading: IconButton(
-          icon: Icon(Icons.arrow_back),
+          icon: Icon(
+            Icons.keyboard_arrow_left,
+            color: Colors.red,
+            size: 24,
+          ),
           onPressed: () {
             Navigator.pop(context); // Kembali ke tampilan sebelumnya
           },
         ),
+        elevation: 4,
       ),
       body: UserProfile(),
     );
@@ -31,79 +42,84 @@ class UserProfile extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Stack(
-            children: [
-              CircleAvatar(
-                radius: 50,
-                backgroundColor: Colors.grey, // Replace with actual image
-              ),
-              Positioned(
-                bottom: 0,
-                right: 0,
-                child: Container(
-                  width: 30,
-                  height: 30,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.white,
-                    border: Border.all(color: Colors.red),
-                  ),
-                  child: Icon(
-                    Icons.edit,
-                    size: 16,
-                    color: Colors.red,
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.red, // Latar belakang merah
+              borderRadius: BorderRadius.circular(
+                  20), // Membuat sudut-sudut menjadi melengkung
+            ),
+            padding: EdgeInsets.all(16), // Padding kontainer
+            child: Row(
+              children: [
+                ClipOval(
+                  child: Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.grey, // Ganti dengan gambar sebenarnya
+                    child: CircleAvatar(
+                      radius: 50,
+                      backgroundColor:
+                          Colors.grey, // Ganti dengan gambar sebenarnya
+                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
-          SizedBox(height: 16),
-          Text(
-            'Tangguh Laksana',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
+                SizedBox(width: 16), // Jarak antara avatar dan teks
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Tangguh Laksana',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Text(
+                      '1302210025',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.normal,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Text(
+                      'Mahasiswa',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.normal,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
-          Text(
-            '1302210025',
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.normal,
-              color: Colors.grey,
-            ),
-          ),
-          Text(
-            'Mahasiswa',
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.normal,
-              color: Colors.grey,
-            ),
-          ),
-          //USERNAME
           SizedBox(height: 24),
+          //USERNAME
           Text(
             'Username',
             style: TextStyle(
               fontSize: 16,
               fontWeight: FontWeight.bold,
-              color: Colors.black87,
+              color: Colors.black,
             ),
           ),
           Container(
+            margin: EdgeInsets.only(top: 8), // Margin atas
             decoration: BoxDecoration(
-              color: Colors.grey, // White background color
-              borderRadius:
-                  BorderRadius.circular(10.0), // Optional: rounded corners
+              borderRadius: BorderRadius.circular(10.0),
+              border: Border.all(color: Colors.red),
             ),
             child: Padding(
-              padding: EdgeInsets.symmetric(
-                  horizontal: 15.0), // Padding inside the container
+              padding: EdgeInsets.symmetric(horizontal: 15.0),
               child: TextField(
-                // controller: _passwordController,
                 enabled: false,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
                 decoration: InputDecoration(
                   labelText: 'Admin',
                   border: InputBorder.none,
@@ -122,17 +138,19 @@ class UserProfile extends StatelessWidget {
             ),
           ),
           Container(
+            margin: EdgeInsets.only(top: 8), // Margin atas
             decoration: BoxDecoration(
-              color: Colors.grey, // White background color
-              borderRadius:
-                  BorderRadius.circular(10.0), // Optional: rounded corners
+              borderRadius: BorderRadius.circular(10.0),
+              border: Border.all(color: Colors.red),
             ),
             child: Padding(
-              padding: EdgeInsets.symmetric(
-                  horizontal: 15.0), // Padding inside the container
+              padding: EdgeInsets.symmetric(horizontal: 15.0),
               child: TextField(
-                // controller: _passwordController,
                 enabled: false,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
                 decoration: InputDecoration(
                   labelText: 'First Name',
                   border: InputBorder.none,
@@ -151,17 +169,19 @@ class UserProfile extends StatelessWidget {
             ),
           ),
           Container(
+            margin: EdgeInsets.only(top: 8), // Margin atas
             decoration: BoxDecoration(
-              color: Colors.grey, // White background color
-              borderRadius:
-                  BorderRadius.circular(10.0), // Optional: rounded corners
+              borderRadius: BorderRadius.circular(10.0),
+              border: Border.all(color: Colors.red),
             ),
             child: Padding(
-              padding: EdgeInsets.symmetric(
-                  horizontal: 15.0), // Padding inside the container
+              padding: EdgeInsets.symmetric(horizontal: 15.0),
               child: TextField(
-                // controller: _passwordController,
                 enabled: false,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
                 decoration: InputDecoration(
                   labelText: 'Last Name',
                   border: InputBorder.none,
@@ -180,60 +200,28 @@ class UserProfile extends StatelessWidget {
             ),
           ),
           Container(
+            margin: EdgeInsets.only(top: 8), // Margin atas
             decoration: BoxDecoration(
-              color: Colors.grey, // White background color
-              borderRadius:
-                  BorderRadius.circular(10.0), // Optional: rounded corners
+              borderRadius: BorderRadius.circular(10.0),
+              border: Border.all(color: Colors.red),
             ),
             child: Padding(
-              padding: EdgeInsets.symmetric(
-                  horizontal: 15.0), // Padding inside the container
+              padding: EdgeInsets.symmetric(horizontal: 15.0),
               child: TextField(
-                // controller: _passwordController,
                 enabled: false,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
                 decoration: InputDecoration(
                   labelText: 'Class',
                   border: InputBorder.none,
                 ),
               ),
             ),
-          )
+          ),
         ],
       ),
-    );
-  }
-
-  Widget buildInfoField(String label, String value) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          label,
-          style: TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
-          ),
-        ),
-        SizedBox(height: 8),
-        Container(
-          width: double.infinity,
-          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-          decoration: BoxDecoration(
-            color: Colors.grey[200],
-            borderRadius: BorderRadius.circular(8),
-          ),
-          child: Text(
-            value,
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.normal,
-              color: Colors.black,
-            ),
-          ),
-        ),
-        SizedBox(height: 16),
-      ],
     );
   }
 }
